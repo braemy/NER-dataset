f99e6b2f746e4691e63d965a14d6cb9105779813
braemy <raemybaptiste@gmail.com>
2017-06-28 16:10:33 +0200

    collect word that can start sentences collect title (Mr, Mrs)

--- analyse_conll/analyse.py
+++ analyse_conll/analyse.py
@@ -1,14 +1,77 @@
+import sys
+from pprint import pprint
+
+sys.path.append("..")
 import string
 import utils
-with open("../../ner/nerc-conll2003/eng-simplified.train", "r") as file:
-    set_before_per = set()
-    previous_token = ""
-    for line in file:
-        line = line.split()
-        if len(line) == 3 and  line[2] == "B-PER":
-            if previous_token != "" and previous_token not in string.punctuation and previous_token[0].isupper():
-                set_before_per.add(previous_token)
-        previous_token = line[0] if len(line) == 3 else ""
-    print(len(set_before_per))
-    print(set_before_per)
-    utils.pickle_data(set_before_per, "data/personal_titles.p")
+import constants as constants
+from collections import Counter
+
+def personal_titles():
+    with open("/dlabdata1/braemy/CoNLL/eng-simplified.train", "r") as file:
+        before_per = []
+        previous_token = ""
+        previous_label = ""
+        position = 0
+        for line in file:
+            line_list = line.split()
+
+            if position!=1 and len(line_list) == 3 and  line_list[2] == "B-PER":
+                if previous_token != "" and previous_token not in string.punctuation and previous_token[0].isupper():
+                    if previous_token in constants.NOT_TITLE or previous_label == "B-MISC" or utils.is_date(previous_token): # => adjectif like American
+                        print(line, previous_token, previous_label)
+                    else:
+                        before_per.append(previous_token)
+            if not line_list:
+                position = 0
+            else:
+                position += 1
+            previous_token = line_list[0] if len(line_list) == 3 else ""
+            previous_label = line_list[2] if len(line_list) == 3 else ""
+
+        #keep only token that appears at least k times
+        set_before_per = {k for k,v in Counter(before_per).items() if v > 1}
+        print(set_before_per)
+        utils.pickle_data(set_before_per, "/dlabdata1/braemy/CoNLL/personal_titles.p")
+
+def sentence_starter():
+    with open("/dlabdata1/braemy/CoNLL/eng-simplified.train", "r") as file:
+        starter = dict()
+        start = True
+        for line in file:
+            tokens = line.split()
+            if tokens:
+                if start:
+                    word = tokens[0]
+                    label = tokens[2]
+                    if word.isdigit() or not word.istitle() or word.islower() or label != "O":
+                        start = False
+                        continue
+                    if word in string.punctuation:
+                        continue
+                    start = False
+                    if word not in starter:
+                        starter[word] = {'start':1, 'inside':0}
+                    else:
+                        starter[word]['start'] += 1
+            else:
+                start = True
+
+        start = True
+        for line in file:
+            tokens = line.split()
+            if tokens:
+                if start:
+                    start = False
+                    continue
+                word = tokens[0]
+                if word in starter:
+                    starter[word]['inside'] +=1
+            else:
+                start = True
+
+        starter = {k.title() for k,v in starter.items() if v['inside']< 10 and v['start'] > 10}
+        utils.pickle_data(starter, "/dlabdata1/braemy/CoNLL/sentence_starter.p")
+
+if __name__ == '__main__':
+    sentence_starter()
